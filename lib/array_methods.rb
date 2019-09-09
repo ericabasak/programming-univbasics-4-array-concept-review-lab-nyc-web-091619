000a21b86bf8ee53c05712d16291dcf7a6c40acb
Erica <ericabasak@gmail.com>
2019-09-09 02:27:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,10 +6,11 @@ def find_element_index(array, value_to_find)
   
   while i < array.length do
     if array[i] === value_to_find
-      return array.index(value_to_find)
+      return i
     else
       return nil
     end
+    i += 1
   end
 end
 
